3d7b4bcddd226f102e40a8def60d92adcb828547
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2023-07-18 16:56:53 +0200

    Update: move api call to api.js, set env port

--- server.js
+++ server.js
@@ -1,83 +1,39 @@
 const express = require('express');
 const axios = require('axios');
+const api = require("./api");
 
 const app = express();
 
 app.use(express.static('public')); // Serve static files from the 'public' folder
-
 app.set('view engine', 'ejs'); // View engine EJS
+
+let token = "";
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/api', (req, res) => {
-  const requestData = {
-    ACTION: "SESSIONTOKEN",
-    MERCHANTUSER: "api.test@payten.com",
-    MERCHANTPASSWORD: "Hephr=R4SKNycaLf",
-    MERCHANT: "chipcardtest01",
-    CUSTOMER: "Customer-UCUoumJV",
-    SESSIONTYPE: "PAYMENTSESSION",
-    MERCHANTPAYMENTID: "Payment-UCUoumJV",
-    AMOUNT: 10.00,
-    CURRENCY: "TRY",
-    CUSTOMEREMAIL: "customerEmail-UCUoumJV",
-    CUSTOMERNAME: "CustomerNameUCUoumJV",
-    CUSTOMERPHONE: 11111111111111,
-    RETURNURL: "http://merchantReturnUrl",
-    SESSIONTYPE: "PAYMENTSESSION",
-    ORDERITEMS: [
-      {
-      "code": "T00D3AITCC",
-      "name": "Galaxy Note 3",
-      "description": "Description of Galaxy Note 3",
-      "quantity": 2,
-      "amount": 449.99
-      },
-      {
-      "code": "B00D9AVYBM",
-      "name": "Samsung Galaxy S III",
-      "description": "Samsung Galaxy S III (S3) Triband White (Boost Mobile)",
-      "quantity": 1,
-      "amount": 149.95
-      },
-      {
-      "code": "B00NQGP5M8",
-      "name": "Apple iPhone 6",
-      "description": "Apple iPhone 6, Gold, 64 GB (Unlocked) by Apple",
-      "quantity": 1,
-      "amount": 139.95
-      },
-      {
-      "code": "B00U8KSUIG",
-      "name": "Samsung Galaxy S6",
-      
-      "description": "Samsung Galaxy S6 SM-G920F 32GB (FACTORY UNLOCKED) 5.1 QHDBlack-InternationalVersion",
-      
-      "quantity": 1,
-      "amount": 129.95
-      }
-    ]
-  };
 
+app.get('/payment', async (req, res) => {
+  try {
+    const responseData = await api.apiSessionTokenRequest();
+    token = responseData["sessionToken"];
+    const url = "https://entegrasyon.asseco-see.com.tr/chipcard/pay3d/" + token;
+    console.log(token);
 
-  axios.post('https://entegrasyon.asseco-see.com.tr/msu/api/v2', requestData, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  })
-    .then(response => {
-      res.json(response.data);
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).json({ error: 'Error occurred' });
-    });
+    res.redirect(url);
+  } catch (error) {
+    res.status(500).json({ error: 'Error with API call' })
+  }
+});
+
+app.post('/success', async (req, res) => {
+  //console.log("request", await api.apiTransactionStatusCheck(token));
+  //console.log("reqBody " + req.getParameter('apiMerchantId'));
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3030;
-
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
